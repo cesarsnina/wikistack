7c1b9b274242b41722b740ef598ef0f9f018212b
Cesar Nina <c3sar207@gmail.com>
2020-03-12 11:42:04 -0400

    added routes

---
+++ public/app.js
@@ -0,0 +1,33 @@
+const express = require('express').Router();
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'))
+
+app.use(express.static(__dirname + '/public'));
+
+// parses url-encoded bodies
+app.use(express.urlencoded({ extended: false }));
+
+// parses json bodies
+app.use(express.json())
+
+//CRUD create, read, update, delete
+// get, post, put, delete - routes
+
+//request, response
+app.get('/', (req, res, next) => {
+  try {
+      res.send('Hello World')
+  } catch (error){
+      console.log('error: ', error)
+      next(error)
+    }
+})
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
